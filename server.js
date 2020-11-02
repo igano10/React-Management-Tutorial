3a72a907ba5c75096d87be8ebe07c52ac0ead28b
igano10 <igano10@gmail.com>
2020-11-02 21:19:44 +0900

    connect database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,32 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect((err) => {
+    if(err){
+        console.log('Error connection to DB');
+        console.log(err);
+        return;
+    }
+});
+
 app.get('/api/customers', (req, res) => {
-    res.send(
-        [{
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '김유신',
-            'birthday': '951201',
-            'gender': '남자',
-            'job': '개발자'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '이순신',
-            'birthday': '980106',
-            'gender': '남자',
-            'job': '디자이너'
-          }
-          ]
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
     );
 });
 
